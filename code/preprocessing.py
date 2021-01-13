94630153c0874d80ef66b07d482842654c4d456a
Khaled <khaled.mohamed98@eng-st.cu.edu.eg>
2021-01-13 14:11:54 +0200

    preprocessing edited

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -12,9 +12,11 @@ import numpy as np
 
 #takes image and return cropped image without noise and cropped binary image without noise
 def preprocessing(img):
-    #img = cv2.imread("3.png",0)
+
+    #crop noise on borders
+	img= img[int(img.shape[0]*0.04):int(img.shape[0]*0.96),int(img.shape[1]*0.04):int(img.shape[0]*0.96)]
     
-    #blur to remove noise
+	#blur to remove noise
     img =  cv2.medianBlur(img,5)
         
     #black and white 
@@ -27,30 +29,33 @@ def preprocessing(img):
     #get contours
     im2,contours, hierarchy = cv2.findContours(binaryImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         
-    Ymin = img.shape[1]
-    Ymax = 0
+    Ymin = 0
+	Ymax = img.shape[0]
+
     for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)
-            if w < img.shape[1]*0.8 or y<img.shape[1]*0.2:
-                continue
-            Ymin = min(Ymin,(y+h))
-            Ymax = max(Ymax,(y-h))
+			x, y, w, h = cv2.boundingRect(contour)
+			if w < img.shape[1]*0.5 or h >img.shape[0]*0.02 :
+				continue
+
+			if y <= img.shape[0]*0.5:         
+				Ymin = max(Ymin,int(y+h))
+			else:
+				Ymax = min(Ymax,int(y-h))
     
     
-    #crop both
-    img = img[Ymin:Ymax,:]
-    binaryImg=binaryImg[Ymin:Ymax,:]
+	#crop both
+	if(Ymin<Ymax):
+		img = img[Ymin:Ymax,:]
+		binaryImg=binaryImg[Ymin:Ymax,:]
+
     
     #remove white borders
     cord=np.where(binaryImg==0)
     img = img[min(cord[0]):max(cord[0]),min(cord[1]):max(cord[1])]
     binaryImg = binaryImg[min(cord[0]):max(cord[0]),min(cord[1]):max(cord[1])]
     
-      
+    #return cropped img,cropped binaryImg  
     return img,binaryImg
     
     
-    #cv2.imwrite('binaryImg.png',binaryImg)
-    
-    
-    #cv2.imwrite('img.png',img)
+
